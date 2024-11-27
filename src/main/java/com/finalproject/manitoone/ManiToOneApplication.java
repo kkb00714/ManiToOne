8926b64ca8c26cb4985961e5c8302c2ec16da9ea
kkb <0furiousblade0@gmail.com>
2024-11-27 14:11:04 +0900

    feat : 프로젝트 생성

---
+++ src/main/java/com/finalproject/manitoone/ManiToOneApplication.java
@@ -0,0 +1,13 @@
+package com.finalproject.manitoone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManiToOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManiToOneApplication.class, args);
+    }
+
+}

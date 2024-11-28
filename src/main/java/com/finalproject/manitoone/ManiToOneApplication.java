3e0c6f5b3cafd25b3da65981be92bf9b21d3645d
sqrt3 <1dimchae@gmail.com>
2024-11-28 09:29:41 +0900

    chore: 깃헙 테스트용 커밋

--- src/main/java/com/finalproject/manitoone/ManiToOneApplication.java
+++ src/main/java/com/finalproject/manitoone/ManiToOneApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ManiToOneApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ManiToOneApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(ManiToOneApplication.class, args);
+  }
 
 }

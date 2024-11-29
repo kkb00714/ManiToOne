158a2995cbdd66b8d3e8a717b8c4b63f5d2111c8
kkb <0furiousblade0@gmail.com>
2024-11-29 13:27:05 +0900

    feat : UserRepository 생성 #21

--- src/main/java/com/finalproject/manitoone/repository/UserRepository.java
+++ src/main/java/com/finalproject/manitoone/repository/UserRepository.java
@@ -8,5 +8,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-  Optional<User> findUserByNickname(String nickname);
+  Optional<User> findByEmail(String email);
+
+  Optional<User> findByNickname(String nickname);
 }

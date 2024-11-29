cbaa5eeca4f07504c2449c69d1be0c920f794080
WeeYoungSeok <dndrldzk@gmail.com>
2024-11-29 15:34:05 +0900

    feat : 읽지 않은 나의 알림 가져오는 메서드 구현
    Fixes: #16

--- src/main/java/com/finalproject/manitoone/service/NotificationService.java
+++ src/main/java/com/finalproject/manitoone/service/NotificationService.java
@@ -1,10 +1,12 @@
 package com.finalproject.manitoone.service;
 
 import com.finalproject.manitoone.constants.NotiType;
+import com.finalproject.manitoone.domain.Notification;
 import com.finalproject.manitoone.domain.User;
 import com.finalproject.manitoone.domain.dto.AddNotificationRequest;
 import com.finalproject.manitoone.domain.dto.NotificationResponse;
 import com.finalproject.manitoone.repository.NotificationRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,7 +16,8 @@ public class NotificationService {
 
   private final NotificationRepository notificationRepository;
 
-  public AddNotificationRequest createAddNotificationRequest(User user, NotiType type, Long relatedObjectId) {
+  public AddNotificationRequest createAddNotificationRequest(User user, NotiType type,
+      Long relatedObjectId) {
     return AddNotificationRequest.builder()
         .user(user)
         .type(type)
@@ -22,8 +25,13 @@ public class NotificationService {
         .build();
   }
 
-
   public NotificationResponse createNotification(AddNotificationRequest addNotificationRequest) {
     return notificationRepository.save(addNotificationRequest.toEntity()).toResponse();
+  }
+
+  public List<NotificationResponse> getAllUnReadNotifications(User user) {
+    return notificationRepository.findByIsReadAndUser(false, user).stream()
+        .map(Notification::toResponse)
+        .toList();
   }
 }

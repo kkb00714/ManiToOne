c7b42dd74854c8a7c4e0ec9c4731b93c6406a08a
WeeYoungSeok <dndrldzk@gmail.com>
2024-11-29 13:40:05 +0900

    rename: controller, repo, service 클래스명 변경
    Fixes: #16

--- src/main/java/com/finalproject/manitoone/notifications/repository/NotificationRepository.java
+++ src/main/java/com/finalproject/manitoone/notifications/repository/NotificationRepository.java
@@ -1,10 +1,10 @@
 package com.finalproject.manitoone.notifications.repository;
 
-import com.finalproject.manitoone.notifications.domain.Notifications;
+import com.finalproject.manitoone.notifications.domain.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface NotificationsRepository extends JpaRepository<Notifications, Long> {
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
 }

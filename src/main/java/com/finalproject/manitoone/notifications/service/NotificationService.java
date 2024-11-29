fa3ff52bcfdbc026e9c066dc629605863b43fd43
WeeYoungSeok <dndrldzk@gmail.com>
2024-11-29 14:17:42 +0900

    feat : 알림 Request 생성 메소드 구현
    Fixes: #16

--- src/main/java/com/finalproject/manitoone/notifications/service/NotificationService.java
+++ src/main/java/com/finalproject/manitoone/notifications/service/NotificationService.java
@@ -1,8 +1,22 @@
 package com.finalproject.manitoone.notifications.service;
 
+import com.finalproject.manitoone.notifications.constants.NotiType;
+import com.finalproject.manitoone.notifications.domain.dto.AddNotificationRequest;
+import com.finalproject.manitoone.notifications.repository.NotificationRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class NotificationService {
 
+  private final NotificationRepository notificationRepository;
+
+  public AddNotificationRequest createAddNotificationRequest(Long userId, NotiType type, Long relatedObjectId) {
+    return AddNotificationRequest.builder()
+        .userId(userId)
+        .type(type)
+        .relatedObjectId(relatedObjectId)
+        .build();
+  }
 }

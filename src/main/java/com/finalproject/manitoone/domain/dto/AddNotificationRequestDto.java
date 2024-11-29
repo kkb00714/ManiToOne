acb73b8b5f3e290781b6861a39d8aa8ff8847e38
WeeYoungSeok <dndrldzk@gmail.com>
2024-11-29 16:38:30 +0900

    rename : 클래스명 수정 Dto 붙임
    Fixes: #16

--- src/main/java/com/finalproject/manitoone/domain/dto/AddNotificationRequestDto.java
+++ src/main/java/com/finalproject/manitoone/domain/dto/AddNotificationRequestDto.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class AddNotificationRequest {
+public class AddNotificationRequestDto {
 
 //  private User receiveUser;
 //  private User sendUser;

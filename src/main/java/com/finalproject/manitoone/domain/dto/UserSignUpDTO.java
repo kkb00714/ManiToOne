127b9bf767b6166a406cf467eab91424e4034089
kkb <0furiousblade0@gmail.com>
2024-11-29 14:12:46 +0900

    refactor : birth 데이터 타입을 LocalDateTime에서 LocalDate로 변경 #21

--- src/main/java/com/finalproject/manitoone/domain/dto/UserSignUpDTO.java
+++ src/main/java/com/finalproject/manitoone/domain/dto/UserSignUpDTO.java
@@ -1,9 +1,10 @@
 package com.finalproject.manitoone.domain.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -33,10 +34,10 @@ public class UserSignUpDTO {
   @NotBlank(message = "이름은 필수 입력값입니다.")
   private String name;
 
-  @Size(min = 2, max = 10, message ="닉네임은 2~10자리로 설정해야 합니다.")
+  @Size(min = 2, max = 10, message = "닉네임은 2~10자리로 설정해야 합니다.")
   @NotBlank(message = "닉네임은 필수 입력값입니다.")
   private String nickname;
 
-  @NotBlank(message = "생년월일은 필수 입력값입니다.")
-  private LocalDateTime birth;
+  @NotNull(message = "생년월일은 필수 입력값입니다.")
+  private LocalDate birth;
 }

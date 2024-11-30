7f55515b4e144bf90cef07d3c690a5fc82cfc93c
YoonjongJ <yoonjong.jang.123@gmail.com>
2024-11-30 18:37:25 +0900

    feat: 게시글 작성 DTO 클래스 생성

---
+++ src/main/java/com/finalproject/manitoone/domain/dto/AddPostRequestDto.java
@@ -0,0 +1,18 @@
+package com.finalproject.manitoone.domain.dto;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AddPostRequestDto {
+  private String content;
+  private Boolean isManito;
+  private List<MultipartFile> images;
+}

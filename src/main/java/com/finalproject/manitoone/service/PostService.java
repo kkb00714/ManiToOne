ab801e23e5d200413cdc4f6521145065f9156248
YoonjongJ <yoonjong.jang.123@gmail.com>
2024-12-02 16:28:45 +0900

    feat: PostService - 게시글 생성 기능 추가

---
+++ src/main/java/com/finalproject/manitoone/service/PostService.java
@@ -0,0 +1,63 @@
+package com.finalproject.manitoone.service;
+
+import com.finalproject.manitoone.domain.Post;
+import com.finalproject.manitoone.domain.PostImage;
+import com.finalproject.manitoone.domain.dto.AddPostRequestDto;
+import com.finalproject.manitoone.domain.dto.PostResponseDto;
+import com.finalproject.manitoone.repository.PostImageRepository;
+import com.finalproject.manitoone.repository.PostRepository;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+
+  private final PostRepository postRepository;
+  private final PostImageRepository postImageRepository;
+
+  // 게시글 생성
+  public PostResponseDto createPost(AddPostRequestDto request) throws IOException {
+    // text 저장
+    Post post = postRepository.save(request.toEntity());
+
+    // 이미지 저장
+    List<MultipartFile> images = request.getImages();
+
+    if (images != null && !images.isEmpty()) {
+      for (MultipartFile image : images) {
+        saveImage(post, image);
+      }
+    }
+
+    return post.toPostResponseDto();
+  }
+
+  // 이미지 저장
+  private void saveImage(Post post, MultipartFile image) throws IOException {
+    // 파일 저장 경로 지정
+    String uploadDir = "src/main/resources/static/img/upload/";
+    Path uploadPath = Paths.get(uploadDir);
+    if (!Files.exists(uploadPath)) {
+      Files.createDirectories(uploadPath);
+    }
+
+    // 이미지 저장
+    String originalFilename = image.getOriginalFilename();
+    String uniqueFileName = UUID.randomUUID() + "-" + originalFilename;
+    Path filePath = uploadPath.resolve(uniqueFileName);
+    Files.write(filePath, image.getBytes());
+
+    postImageRepository.save(PostImage.builder()
+        .fileName(originalFilename)
+        .post(post)
+        .build());
+  }
+}

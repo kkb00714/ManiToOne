0090075730708466d35233067c5fbdb8195da77e
YoonjongJ <yoonjong.jang.123@gmail.com>
2024-12-02 11:27:44 +0900

    refactor: AddPostRequest DTO 클래스 수정

--- src/main/java/com/finalproject/manitoone/domain/dto/AddPostRequestDto.java
+++ src/main/java/com/finalproject/manitoone/domain/dto/AddPostRequestDto.java
@@ -1,10 +1,12 @@
 package com.finalproject.manitoone.domain.dto;
 
 import com.finalproject.manitoone.domain.Post;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
 
 @Getter
 @NoArgsConstructor
@@ -14,6 +16,7 @@ public class AddPostRequestDto {
 
   private String content;
   private Boolean isManito;
+  List<MultipartFile> images;
 
   public Post toEntity() {
     return Post.builder()

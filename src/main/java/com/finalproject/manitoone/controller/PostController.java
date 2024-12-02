29b9cf33d5e1bfa35b87b177f142349f51f94454
YoonjongJ <yoonjong.jang.123@gmail.com>
2024-12-03 01:03:18 +0900

    feat: PostController - 게시글 생성 기능 추가

--- src/main/java/com/finalproject/manitoone/controller/PostController.java
+++ src/main/java/com/finalproject/manitoone/controller/PostController.java
@@ -1,13 +1,20 @@
 package com.finalproject.manitoone.controller;
 
+import com.finalproject.manitoone.domain.User;
+import com.finalproject.manitoone.domain.dto.AddPostRequestDto;
+import com.finalproject.manitoone.domain.dto.PostResponseDto;
 import com.finalproject.manitoone.dto.post.PostResponseDto;
 import com.finalproject.manitoone.service.PostService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +26,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class PostController {
 
   private final PostService postService;
+
+  // 게시글 생성
+  @PostMapping
+  public ResponseEntity<PostResponseDto> createPost(@ModelAttribute AddPostRequestDto request,
+      @AuthenticationPrincipal User user) {
+    try {
+      postService.createPost(request, user);
+      return ResponseEntity.status(HttpStatus.CREATED).build();
+    } catch (Exception e) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+  }
 
   @GetMapping("/by/{nickName}")
   public ResponseEntity<List<PostResponseDto>> getPostsByUserId(@PathVariable String nickName,

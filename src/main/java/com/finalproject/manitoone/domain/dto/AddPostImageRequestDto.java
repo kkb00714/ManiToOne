11d397b41eea7d5efbf53546381fc152697ecd69
YoonjongJ <yoonjong.jang.123@gmail.com>
2024-12-02 11:24:46 +0900

    feat: AddPostImageRequest DTO 클래스 생성

---
+++ src/main/java/com/finalproject/manitoone/domain/dto/AddPostImageRequestDto.java
@@ -0,0 +1,17 @@
+package com.finalproject.manitoone.domain.dto;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AddPostImageRequestDto {
+
+  List<MultipartFile> images;
+}

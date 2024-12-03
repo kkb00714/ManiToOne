09652c8b9709098253ce1d25db6feddaaf1c96d1
sqrt3 <1dimchae@gmail.com>
2024-12-03 15:44:53 +0900

    fix: 닉네임으로 유저를 가져오는 JPA 메서드 통일

--- src/main/java/com/finalproject/manitoone/repository/UserRepository.java
+++ src/main/java/com/finalproject/manitoone/repository/UserRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-  Optional<User> findByEmail(String email);
+  Optional<User> findUserByNickname(String nickname);
 
-  Optional<User> findByNickname(String nickname);
+  Optional<User> findByEmail(String email);
 }

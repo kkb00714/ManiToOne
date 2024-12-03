23094972c7bfb4bad09f24a93a180e119cd9830d
sqrt3 <1dimchae@gmail.com>
2024-12-03 11:32:34 +0900

    refactor: dto 이름 변경으로 인한 코드 수정

--- src/main/java/com/finalproject/manitoone/controller/PostController.java
+++ src/main/java/com/finalproject/manitoone/controller/PostController.java
@@ -3,7 +3,7 @@ package com.finalproject.manitoone.controller;
 import com.finalproject.manitoone.domain.User;
 import com.finalproject.manitoone.domain.dto.AddPostRequestDto;
 import com.finalproject.manitoone.domain.dto.PostResponseDto;
-import com.finalproject.manitoone.dto.post.PostResponseDto;
+import com.finalproject.manitoone.dto.post.PostViewResponseDto;
 import com.finalproject.manitoone.service.PostService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -40,19 +40,19 @@ public class PostController {
   }
 
   @GetMapping("/by/{nickName}")
-  public ResponseEntity<List<PostResponseDto>> getPostsByUserId(@PathVariable String nickName,
+  public ResponseEntity<List<PostViewResponseDto>> getPostsByUserId(@PathVariable String nickName,
       @PageableDefault(sort = "postId", direction = Sort.Direction.DESC) Pageable pageable) {
     return ResponseEntity.ok(postService.getPostsByNickName(nickName, pageable));
   }
 
   @GetMapping("/{nickName}/liked")
-  public ResponseEntity<List<PostResponseDto>> getLikedPostsByUserId(@PathVariable String nickName,
+  public ResponseEntity<List<PostViewResponseDto>> getLikedPostsByUserId(@PathVariable String nickName,
       @PageableDefault(sort = "postId", direction = Sort.Direction.DESC) Pageable pageable) {
     return ResponseEntity.ok(postService.getLikePostByNickName(nickName, pageable));
   }
 
   @GetMapping("/hidden")
-  public ResponseEntity<List<PostResponseDto>> getPostById(
+  public ResponseEntity<List<PostViewResponseDto>> getPostById(
       @PageableDefault(sort = "postId", direction = Sort.Direction.DESC) Pageable pageable) {
     return ResponseEntity.ok(postService.getMyHiddenPosts("테스트1", pageable));
   }

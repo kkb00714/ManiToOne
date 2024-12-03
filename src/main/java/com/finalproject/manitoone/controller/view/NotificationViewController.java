3d341844060392df4c26244e1b42e78df8794dad
WeeYoungSeok <dndrldzk@gmail.com>
2024-12-03 13:39:47 +0900

    feat : 알림을 가져오는 컨트롤러 구현
    Fixes: #32

--- src/main/java/com/finalproject/manitoone/controller/view/NotificationViewController.java
+++ src/main/java/com/finalproject/manitoone/controller/view/NotificationViewController.java
@@ -1,8 +1,25 @@
 package com.finalproject.manitoone.controller.view;
 
+import com.finalproject.manitoone.service.NotificationService;
+import com.finalproject.manitoone.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/noti")
+@RequiredArgsConstructor
 public class NotificationViewController {
 
+  private final NotificationService notificationService;
+  private final UserService userService;
+
+  @GetMapping("/{nickname}")
+  public String getNotifications(@PathVariable String nickname, Model model) {
+    model.addAttribute("notifications", notificationService.getAllUnReadNotifications(nickname));
+    return "fragments/content/notification :: notification"; // 타임리프 Fragment 반환
+  }
 }

9a4fb754219b3fc4bdd09c558f071e0a9d9e5894
sqrt3 <1dimchae@gmail.com>
2024-12-03 11:14:17 +0900

    feat: 유저가 좋아요를 누른 게시글을 가져오는 JPA 작성

---
+++ src/main/java/com/finalproject/manitoone/repository/UserPostLikeRepository.java
@@ -0,0 +1,13 @@
+package com.finalproject.manitoone.repository;
+
+import com.finalproject.manitoone.domain.UserPostLike;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserPostLikeRepository extends JpaRepository<UserPostLike, Long> {
+
+  Optional<List<UserPostLike>> findAllByUser_nicknameAndPost_IsHiddenFalseAndPost_IsBlindFalse(
+      String nickName, Pageable pageable);
+}

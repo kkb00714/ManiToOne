87c80d410c71b02edf49eb91eb73d83b451b7b09
kkb <0furiousblade0@gmail.com>
2024-12-03 15:37:23 +0900

    feat : 비밀번호 암호화 기능 추가 및 이메일 인증 이후 회원가입이 가능한 기능 추가 #35

--- src/main/java/com/finalproject/manitoone/service/UserAuthService.java
+++ src/main/java/com/finalproject/manitoone/service/UserAuthService.java
@@ -15,32 +15,34 @@ public class UserAuthService {
 
   private final UserRepository userRepository;
   private final PasswordEncoder passwordEncoder;
+  private final MailService mailService;
 
   @Transactional
   public String registerUser(UserSignUpDTO userSignUpDTO) {
-    // DTO 내부 검증 호출 - 비밀번호 검증
-    userSignUpDTO.validatePasswords();
+    String email = userSignUpDTO.getEmail();
 
-    // 이메일 중복 체크
-    Optional<User> existUserByEmail = userRepository.findByEmail(userSignUpDTO.getEmail());
-    if (existUserByEmail.isPresent()) {
-      throw new IllegalArgumentException("이메일이 이미 사용중입니다.");
+    // 1. 이메일 인증 여부 확인
+    if (!mailService.isVerified(email)) {
+      throw new IllegalArgumentException("이메일 인증이 완료되지 않았습니다.");
     }
 
-    // 닉네임 중복 체크
+    // 2. DTO 내부 검증 호출 - 비밀번호 검증
+    userSignUpDTO.validatePasswords();
+
+    // 3. 닉네임 중복 체크
     Optional<User> existUserByNickname = userRepository.findUserByNickname(
         userSignUpDTO.getNickname());
     if (existUserByNickname.isPresent()) {
       throw new IllegalArgumentException("닉네임이 이미 사용중입니다.");
     }
 
-    // 비밀번호 암호화
+    // 4. 비밀번호 암호화 후 회원 저장
     String encryptedPassword = passwordEncoder.encode(userSignUpDTO.getPassword());
-
-    // 중복이 없으면 회원가입 진행
     User newUser = userSignUpDTO.toEntity(encryptedPassword);
-
     userRepository.save(newUser);
+
+    // 5. 인증 완료 상태 제거
+    mailService.removeVerifiedEmail(email);
     return "회원가입이 완료됐습니다";
   }
 }

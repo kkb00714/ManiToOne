42e47451c43501e1ceccd5d7edbdc73f40c67ab3
WeeYoungSeok <dndrldzk@gmail.com>
2024-12-04 15:14:49 +0900

    docs : 알림 생성 주석 작성
    Fixes: #32 #50

--- src/main/java/com/finalproject/manitoone/util/NotificationUtil.java
+++ src/main/java/com/finalproject/manitoone/util/NotificationUtil.java
@@ -17,6 +17,15 @@ public class NotificationUtil {
   private final NotificationRepository notificationRepository;
   private final UserRepository userRepository;
 
+  /**
+   * 특정 사용자에게 알림을 생성합니다.
+   *
+   * @param receiveUserNickname 알림을 받을 사용자의 닉네임 (예: 팔로우를 받은 유저나 게시글의 주인 등)
+   * @param sendUser 알림을 보낸 사용자 (예: 게시글에 좋아요를 누르거나 댓글을 작성한 사용자)
+   * @param type 알림의 유형 (예: 게시글 댓글, 팔로우, 좋아요 등, NotiType Enum 참고)
+   * @param relatedObjectId 알림을 클릭했을 때 이동할 페이지의 대상 ID (예: 게시글 ID, 사용자 ID 등)
+   * @return 생성된 알림 정보를 포함하는 {@link NotificationResponseDto}
+   */
   public NotificationResponseDto createNotification(
       String receiveUserNickname,
       User sendUser, NotiType type,

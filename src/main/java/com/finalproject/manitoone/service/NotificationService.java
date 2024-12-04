89c1ba838ed5352b82cb9cec2b26b0bb504bd8da
WeeYoungSeok <dndrldzk@gmail.com>
2024-12-04 16:14:14 +0900

    refactor : Dto 내부 로직 서비스로 이전
    Fixes: #32 #50

--- src/main/java/com/finalproject/manitoone/service/NotificationService.java
+++ src/main/java/com/finalproject/manitoone/service/NotificationService.java
@@ -5,6 +5,7 @@ import com.finalproject.manitoone.domain.User;
 import com.finalproject.manitoone.domain.dto.NotificationResponseDto;
 import com.finalproject.manitoone.repository.NotificationRepository;
 import com.finalproject.manitoone.repository.UserRepository;
+import com.finalproject.manitoone.util.DataUtil;
 import jakarta.servlet.http.HttpSession;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +18,8 @@ public class NotificationService {
   private final NotificationRepository notificationRepository;
   private final UserRepository userRepository;
 
+  private final DataUtil dataUtil;
+
   public List<NotificationResponseDto> getAllUnReadNotifications(HttpSession session) {
     User user = (User) session.getAttribute("user");
     if (user == null) {
@@ -26,14 +29,22 @@ public class NotificationService {
             userRepository.findUserByNickname(user.getNickname())
                 .orElseThrow(() -> new IllegalArgumentException("해당 닉네임을 가진 유저를 찾을 수 없습니다."))).stream()
         .map(notification -> {
-          NotificationResponseDto notificationResponseDto = new NotificationResponseDto(
-              notification);
+          NotificationResponseDto notificationResponseDto = NotificationResponseDto.builder()
+              .notiId(notification.getNotiId())
+              .relatedObjectId(notification.getRelatedObjectId())
+              .isRead(notification.getIsRead())
+              .createdAt(notification.getCreatedAt())
+              .timeDifference(dataUtil.getTimeDifference(notification.getCreatedAt()))
+              .type(notification.getType())
+              .build();
+          // FIXME: setter를 사용하지 않는 방식으로 content 설정 로직 리팩토링 필요
           if (notificationResponseDto.getType().requiresUserName()) {
             User sendUser = userRepository.findById(notificationResponseDto.getRelatedObjectId())
                 .orElseThrow(() -> new IllegalArgumentException("알림을 보낸 유저를 찾을 수 없습니다."));
-            notificationResponseDto.setContent(sendUser.getNickname());
+            notificationResponseDto.setContent(
+                notificationResponseDto.getType().getMessage(sendUser.getNickname()));
           } else {
-            notificationResponseDto.setContent(null);
+            notificationResponseDto.setContent(notificationResponseDto.getType().getMessage(null));
           }
           return notificationResponseDto;
         })

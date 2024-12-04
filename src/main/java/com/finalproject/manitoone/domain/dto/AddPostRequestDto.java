8e428dcdc68742f54121f72c9eb9fec831c00aa8
YoonjongJ <yoonjong.jang.123@gmail.com>
2024-12-05 01:42:43 +0900

    refactor: AddPostRequstDto 수정

--- src/main/java/com/finalproject/manitoone/domain/dto/AddPostRequestDto.java
+++ src/main/java/com/finalproject/manitoone/domain/dto/AddPostRequestDto.java
@@ -16,7 +16,7 @@ public class AddPostRequestDto {
 
   private String content;
   private Boolean isManito;
-  List<MultipartFile> images;
+  // List<MultipartFile> images;
 
   public Post toEntity() {
     return Post.builder()

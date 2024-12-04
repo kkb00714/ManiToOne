405ad5cad1f51b12fbbd96cb48c292f42247e8d1
WeeYoungSeok <dndrldzk@gmail.com>
2024-12-04 15:19:33 +0900

    feat : AdminViewController 클래스 뼈대 생성
    Fixes: #41

---
+++ src/main/java/com/finalproject/manitoone/controller/view/AdminViewController.java
@@ -0,0 +1,9 @@
+package com.finalproject.manitoone.controller.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminViewController {
+}

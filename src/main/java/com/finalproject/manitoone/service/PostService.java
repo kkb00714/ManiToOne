2016153525ce5c84d542cd9a662d4886088c83a1
YoonjongJ <yoonjong.jang.123@gmail.com>
2024-12-05 01:48:40 +0900

    refactor: PostService 수정

--- src/main/java/com/finalproject/manitoone/service/PostService.java
+++ src/main/java/com/finalproject/manitoone/service/PostService.java
@@ -34,48 +34,53 @@ public class PostService {
   private final ReplyPostRepository replyPostRepository;
 
   // 게시글 생성
-  public PostResponseDto createPost(AddPostRequestDto request, User user) throws IOException {
-    // text 저장
-    Post post = Post.builder()
-        .user(user)
-        .content(request.getContent())
-        .isManito(request.getIsManito())
-        .build();
-
-    postRepository.save(post);
-
-    // 이미지 저장
-    List<MultipartFile> images = request.getImages();
-
-    if (images != null && !images.isEmpty()) {
-      for (MultipartFile image : images) {
-        saveImage(post, image);
-      }
-    }
-
-    return post.toPostResponseDto();
+  public PostResponseDto createPost(AddPostRequestDto request) {
+    Post post = postRepository.save(request.toEntity());
+    return new PostResponseDto(post.getPostId(), post.getContent(), post.getIsManito());
   }
+
+//  public PostResponseDto createPost(AddPostRequestDto request, User user) throws IOException {
+//    // text 저장
+//    Post post = Post.builder()
+//        .user(user)
+//        .content(request.getContent())
+//        .isManito(request.getIsManito())
+//        .build();
+//
+//    postRepository.save(post);
+//
+//    // 이미지 저장
+//    List<MultipartFile> images = request.getImages();
+//
+//    if (images != null && !images.isEmpty()) {
+//      for (MultipartFile image : images) {
+//        saveImage(post, image);
+//      }
+//    }
+//
+//    return post.toPostResponseDto();
+//  }
 
   // 이미지 저장
-  private void saveImage(Post post, MultipartFile image) throws IOException {
-    // 파일 저장 경로 지정
-    String uploadDir = "src/main/resources/static/img/upload/";
-    Path uploadPath = Paths.get(uploadDir);
-    if (!Files.exists(uploadPath)) {
-      Files.createDirectories(uploadPath);
-    }
-
-    // 이미지 저장
-    String originalFilename = image.getOriginalFilename();
-    String uniqueFileName = UUID.randomUUID() + "-" + originalFilename;
-    Path filePath = uploadPath.resolve(uniqueFileName);
-    Files.write(filePath, image.getBytes());
-
-    postImageRepository.save(PostImage.builder()
-        .fileName(originalFilename)
-        .post(post)
-        .build());
-  }
+//  private void saveImage(Post post, MultipartFile image) throws IOException {
+//    // 파일 저장 경로 지정
+//    String uploadDir = "src/main/resources/static/img/upload/";
+//    Path uploadPath = Paths.get(uploadDir);
+//    if (!Files.exists(uploadPath)) {
+//      Files.createDirectories(uploadPath);
+//    }
+//
+//    // 이미지 저장
+//    String originalFilename = image.getOriginalFilename();
+//    String uniqueFileName = UUID.randomUUID() + "-" + originalFilename;
+//    Path filePath = uploadPath.resolve(uniqueFileName);
+//    Files.write(filePath, image.getBytes());
+//
+//    postImageRepository.save(PostImage.builder()
+//        .fileName(originalFilename)
+//        .post(post)
+//        .build());
+//  }
 
   public List<PostViewResponseDto> getPostsByNickName(String nickName, Pageable pageable) {
     // TODO: 내 게시글인지는 어떻게 판별할까요?

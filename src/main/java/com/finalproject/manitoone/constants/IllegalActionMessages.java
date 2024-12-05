20185be1509f802fffacf41e7183a9819f4e7ca9
YoonjongJ <yoonjong.jang.123@gmail.com>
2024-12-05 14:18:26 +0900

    feat: IllegalActionMessages - 예외 처리 메시지 추가

--- src/main/java/com/finalproject/manitoone/constants/IllegalActionMessages.java
+++ src/main/java/com/finalproject/manitoone/constants/IllegalActionMessages.java
@@ -9,6 +9,7 @@ public enum IllegalActionMessages {
   CANNOT_FIND_USER_WITH_GIVEN_ID("해당하는 유저 ID를 찾을 수 없습니다."),
   CANNOT_FIND_POST_WITH_GIVEN_ID("해당하는 게시글 ID를 찾을 수 없습니다."),
   CANNOT_FIND_POST_IMAGE_WITH_GIVEN_ID("해당 게시물은 이미지를 포함하고 있지 않습니다."),
+  CANNOT_FINE_REPLY_POST_WITH_GIVEN_ID("해당 게시물은 답글이 없습니다."),
   CANNOT_FIND_USER_BY_GIVEN_NICKNAME("해당하는 유저의 닉네임을 찾을 수 없습니다."),
   CANNOT_GET_FOLLOWERS("팔로워 정보를 가져오는 중 문제가 발생했습니다."),
   CANNOT_GET_FOLLOWING("팔로잉 정보를 가져오는 중 문제가 발생했습니다."),

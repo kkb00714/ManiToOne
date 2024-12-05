0709575794c3d617af23ecbadb0dd77fb94f272f
WeeYoungSeok <dndrldzk@gmail.com>
2024-12-05 09:30:44 +0900

    feat : 어드민 index controller 연결
    Fixes: #41

--- src/main/java/com/finalproject/manitoone/controller/view/AdminViewController.java
+++ src/main/java/com/finalproject/manitoone/controller/view/AdminViewController.java
@@ -1,9 +1,15 @@
 package com.finalproject.manitoone.controller.view;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminViewController {
+
+  @GetMapping
+  public String adminIndex() {
+    return "pages/admin/adminIndex";
+  }
 }

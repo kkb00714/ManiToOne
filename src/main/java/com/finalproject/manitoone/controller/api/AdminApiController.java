19329fb7bb62fb9fe0e1a4a6e94abfd1df836acb
WeeYoungSeok <dndrldzk@gmail.com>
2024-12-05 18:28:58 +0900

    feat : 유저 검색 페이징 controller 구현 (size 임시)
    Fixes: #41

---
+++ src/main/java/com/finalproject/manitoone/controller/api/AdminApiController.java
@@ -0,0 +1,27 @@
+package com.finalproject.manitoone.controller.api;
+
+import com.finalproject.manitoone.domain.dto.admin.UserSearchRequestDto;
+import com.finalproject.manitoone.service.AdminService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin")
+@RequiredArgsConstructor
+public class AdminApiController {
+
+  private final AdminService adminService;
+
+  @PostMapping("/users")
+  public ResponseEntity<Object> getAllUsers(@PageableDefault(size = 2, sort = "userId", direction = Sort.Direction.ASC) Pageable pageable,
+      @RequestBody UserSearchRequestDto userSearchRequestDto) {
+    return ResponseEntity.ok(adminService.searchUsers(userSearchRequestDto, pageable));
+  }
+}

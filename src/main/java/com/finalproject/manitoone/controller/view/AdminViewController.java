419782585734a72a10e8d3508ad8d245a9b98cee
WeeYoungSeok <dndrldzk@gmail.com>
2024-12-05 17:07:26 +0900

    feat : 회원 관리 페이지 이동 맵핑 작성
    Fixes: #41

--- src/main/java/com/finalproject/manitoone/controller/view/AdminViewController.java
+++ src/main/java/com/finalproject/manitoone/controller/view/AdminViewController.java
@@ -1,15 +1,25 @@
 package com.finalproject.manitoone.controller.view;
 
+import com.finalproject.manitoone.service.AdminService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/admin")
+@RequiredArgsConstructor
 public class AdminViewController {
+
+  private final AdminService adminService;
 
   @GetMapping
   public String adminIndex() {
     return "pages/admin/adminIndex";
+  }
+
+  @GetMapping("/users")
+  public String adminUser() {
+    return "pages/admin/adminUser";
   }
 }

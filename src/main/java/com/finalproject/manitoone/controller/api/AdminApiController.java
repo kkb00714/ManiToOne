5ce6c17a0b525f588fc3bd2d985ab11b3f962255
WeeYoungSeok <dndrldzk@gmail.com>
2024-12-06 16:02:19 +0900

    feat : 프로필 업데이트 Controller 뼈대 생성
    Fixes: #41

--- src/main/java/com/finalproject/manitoone/controller/api/AdminApiController.java
+++ src/main/java/com/finalproject/manitoone/controller/api/AdminApiController.java
@@ -1,5 +1,6 @@
 package com.finalproject.manitoone.controller.api;
 
+import com.finalproject.manitoone.domain.dto.admin.UserProfileRequestDto;
 import com.finalproject.manitoone.domain.dto.admin.UserSearchRequestDto;
 import com.finalproject.manitoone.service.AdminService;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +9,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,5 +25,10 @@ public class AdminApiController {
   public ResponseEntity<Object> getAllUsers(@PageableDefault(size = 2, sort = "userId", direction = Sort.Direction.ASC) Pageable pageable,
       @RequestBody UserSearchRequestDto userSearchRequestDto) {
     return ResponseEntity.ok(adminService.searchUsers(userSearchRequestDto, pageable));
+  }
+
+  @PutMapping("/users")
+  public ResponseEntity<Object> updateUsers(@RequestBody UserProfileRequestDto userProfileRequestDto) {
+    return null;
   }
 }

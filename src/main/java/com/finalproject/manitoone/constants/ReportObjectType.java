7449229adc662980543e8c615b843cf5ff212ea1
YoonjongJ <yoonjong.jang.123@gmail.com>
2024-12-06 16:02:48 +0900

    refactor: ReportObjectType 수정

--- src/main/java/com/finalproject/manitoone/constants/ReportObjectType.java
+++ src/main/java/com/finalproject/manitoone/constants/ReportObjectType.java
@@ -6,9 +6,9 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ReportObjectType {
-  // 신고 타입 (에: 게시글 신고)
+  // 신고 타입 (에: 게시글 신고 or 답글 신고 or 마니또 신고)
   POST("게시글"),
-  REPLY("답변"),
+  REPLY("답글"),
   MANITO("마니또");
 
   private final String type;

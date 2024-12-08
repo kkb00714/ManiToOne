707f2fe5c6fe6958b8523a9825f175eb88802961
YoonjongJ <yoonjong.jang.123@gmail.com>
2024-12-09 02:46:25 +0900

    feat: 답글 생성 API 추가

--- src/main/java/com/finalproject/manitoone/controller/api/ReplyController.java
+++ src/main/java/com/finalproject/manitoone/controller/api/ReplyController.java
@@ -1,7 +1,16 @@
 package com.finalproject.manitoone.controller.api;
 
+import com.finalproject.manitoone.domain.User;
+import com.finalproject.manitoone.domain.dto.AddReplyRequestDto;
+import com.finalproject.manitoone.domain.dto.ReplyResponseDto;
 import com.finalproject.manitoone.service.ReplyService;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +20,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class ReplyController {
 
   private final ReplyService replyService;
+
+  // 답글 생성
+  @PostMapping("/{postId}")
+  public ResponseEntity<ReplyResponseDto> createReply(@PathVariable("postId") Long postId,
+      @RequestBody AddReplyRequestDto request,
+      HttpSession session) {
+    User user = (User) session.getAttribute("user");
+    ReplyResponseDto reply = replyService.createReply(postId, request, user);
+    return ResponseEntity.status(HttpStatus.CREATED).body(reply);
+  }
 }
--- src/main/java/com/finalproject/manitoone/service/ReplyService.java
+++ src/main/java/com/finalproject/manitoone/service/ReplyService.java
@@ -1,5 +1,11 @@
 package com.finalproject.manitoone.service;
 
+import com.finalproject.manitoone.constants.IllegalActionMessages;
+import com.finalproject.manitoone.domain.Post;
+import com.finalproject.manitoone.domain.ReplyPost;
+import com.finalproject.manitoone.domain.User;
+import com.finalproject.manitoone.domain.dto.AddReplyRequestDto;
+import com.finalproject.manitoone.domain.dto.ReplyResponseDto;
 import com.finalproject.manitoone.repository.PostRepository;
 import com.finalproject.manitoone.repository.ReplyPostRepository;
 import lombok.RequiredArgsConstructor;
@@ -11,4 +17,21 @@ public class ReplyService {
 
   private final PostRepository postRepository;
   private final ReplyPostRepository replyPostRepository;
+
+  // 답글 생성
+  public ReplyResponseDto createReply(Long postId, AddReplyRequestDto request, User user) {
+    Post post = postRepository.findByPostId(postId)
+        .orElseThrow(() -> new IllegalArgumentException(
+            IllegalActionMessages.CANNOT_FIND_POST_WITH_GIVEN_ID.getMessage()));
+
+    ReplyPost reply = replyPostRepository.save(ReplyPost.builder()
+        .post(post)
+        .user(user)
+        .content(request.getContent())
+        .parentId(post.getPostId())
+        .build());
+
+    return new ReplyResponseDto(reply.getPost(), reply.getUser(), reply.getParentId(),
+        reply.getContent(), reply.getCreatedAt(), reply.getIsBlind());
+  }
 }

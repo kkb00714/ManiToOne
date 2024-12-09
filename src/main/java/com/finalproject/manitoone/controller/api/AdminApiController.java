ed2b06bf54a4ddb91e92304e3cab1cc9421a05cd
WeeYoungSeok <dndrldzk@gmail.com>
2024-12-09 14:37:58 +0900

    style : 유저 정보 가입날짜 표기
    Fixes: #41

--- src/main/java/com/finalproject/manitoone/controller/api/AdminApiController.java
+++ src/main/java/com/finalproject/manitoone/controller/api/AdminApiController.java
@@ -9,11 +9,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/admin")
@@ -34,5 +37,11 @@ public class AdminApiController {
       @RequestBody UserProfileRequestDto userProfileRequestDto) {
     UserProfileResponseDto updatedUser = adminService.updateUser(userProfileRequestDto);
     return ResponseEntity.ok(updatedUser);
+  }
+
+  @PutMapping("/users/{userId}")
+  public ResponseEntity<Object> updateUserProfileImage(@PathVariable Long userId,
+      @RequestPart(required = false) MultipartFile profileImageFile) {
+    return ResponseEntity.ok(adminService.updateProfileImage(userId, profileImageFile));
   }
 }

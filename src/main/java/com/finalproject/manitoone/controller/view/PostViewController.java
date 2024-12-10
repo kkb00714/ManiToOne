9f36dce885f83294df241d962837577d9551184f
YoonjongJ <yoonjong.jang.123@gmail.com>
2024-12-11 02:39:35 +0900

    feat: 게시글 상세 페이지 프론트엔드 작업
    postDetail.html - .post-time까지 타임리프 적용

--- src/main/java/com/finalproject/manitoone/controller/view/PostViewController.java
+++ src/main/java/com/finalproject/manitoone/controller/view/PostViewController.java
@@ -4,6 +4,9 @@ import com.finalproject.manitoone.service.PostService;
 import com.finalproject.manitoone.service.ReplyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,4 +16,11 @@ public class PostViewController {
 
   private final PostService postService;
   private final ReplyService replyService;
+
+  // 게시글 상세 조회
+  @GetMapping("/{postId}")
+  public String getPostDetail(@PathVariable("postId") Long postId, Model model) {
+    model.addAttribute("post", postService.getPostDetail(postId));
+    return "pages/post/postDetail";
+  }
 }

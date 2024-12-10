5dbfadaab35874e278fad1906a4548dc9fd422a4
sqrt3 <1dimchae@gmail.com>
2024-12-10 09:09:44 +0900

    feat: 외부 API를 비동기로 사용하기 위한 비동기 처리 코드 예시 작성

--- src/main/java/com/finalproject/manitoone/controller/UserController.java
+++ src/main/java/com/finalproject/manitoone/controller/UserController.java
@@ -2,8 +2,11 @@ package com.finalproject.manitoone.controller;
 
 import com.finalproject.manitoone.dto.user.UserInformationResponseDto;
 import com.finalproject.manitoone.service.UserService;
+import com.finalproject.manitoone.util.AlanUtil;
+import java.util.concurrent.CompletableFuture;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,8 +19,14 @@ public class UserController {
 
   private final UserService userService;
 
+  //TODO: 확인하셨다면 이 줄과 27번째 줄(System.out)을 지우고 커밋 부탁드립니다.
+  @Async
   @GetMapping("/{nickname}")
-  public ResponseEntity<UserInformationResponseDto> getUserInformation(@PathVariable String nickname) {
-    return ResponseEntity.ok(userService.getUserByNickname(nickname));
+  public CompletableFuture<ResponseEntity<UserInformationResponseDto>> getUserInformation(
+      @PathVariable String nickname) {
+    System.out.println(AlanUtil.getAlanAnswer("나 오늘 치킨 6마리 먹었다"));
+    return CompletableFuture.supplyAsync(
+        () -> ResponseEntity.ok(userService.getUserByNickname(nickname)));
   }
+
 }

73a65becb9649a0c18acd892261554bd721401ee
YoonjongJ <yoonjong.jang.123@gmail.com>
2024-12-11 01:53:27 +0900

    refactor: PostResponseDto - 필드 추가
    createdAt, updatedAt 추가

--- src/main/java/com/finalproject/manitoone/domain/dto/PostResponseDto.java
+++ src/main/java/com/finalproject/manitoone/domain/dto/PostResponseDto.java
@@ -1,6 +1,7 @@
 package com.finalproject.manitoone.domain.dto;
 
 import com.finalproject.manitoone.domain.User;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,5 +16,7 @@ public class PostResponseDto {
   private Long postId;
   private User user;
   private String content;
+  private LocalDateTime createdAt;
+  private LocalDateTime updatedAt;
   private Boolean isManito;
 }

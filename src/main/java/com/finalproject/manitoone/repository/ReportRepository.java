502fc43fc46de2ed2c412b407c47afbeda8f3583
WeeYoungSeok <dndrldzk@gmail.com>
2024-12-11 11:30:04 +0900

    feat : 신고 목록 가져오지 리스트로
    Fixes: #83

--- src/main/java/com/finalproject/manitoone/repository/ReportRepository.java
+++ src/main/java/com/finalproject/manitoone/repository/ReportRepository.java
@@ -2,6 +2,8 @@ package com.finalproject.manitoone.repository;
 
 import com.finalproject.manitoone.constants.ReportObjectType;
 import com.finalproject.manitoone.domain.Report;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,6 @@ import org.springframework.stereotype.Repository;
 public interface ReportRepository extends JpaRepository<Report, Long> {
 
   boolean existsByTypeAndReportObjectId(ReportObjectType type, Long reportObjectId);
+
+  Optional<List<Report>> findAllByTypeAndReportObjectId(ReportObjectType type, Long reportObjectId);
 }

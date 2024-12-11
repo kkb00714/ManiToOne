545f296d2f2ff6c3f6ac7b2c76095985c51bfa56
WeeYoungSeok <dndrldzk@gmail.com>
2024-12-11 09:39:19 +0900

    feat : 게시글 삭제시 신고된 게시글 확인 Repo
    Fixes: #83

--- src/main/java/com/finalproject/manitoone/repository/ReportRepository.java
+++ src/main/java/com/finalproject/manitoone/repository/ReportRepository.java
@@ -1,5 +1,6 @@
 package com.finalproject.manitoone.repository;
 
+import com.finalproject.manitoone.constants.ReportObjectType;
 import com.finalproject.manitoone.domain.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -7,4 +8,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ReportRepository extends JpaRepository<Report, Long> {
 
+  boolean existsByTypeAndReportObjectId(ReportObjectType type, Long reportObjectId);
 }

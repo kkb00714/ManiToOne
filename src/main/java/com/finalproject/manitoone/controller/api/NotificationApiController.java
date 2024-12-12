6747adeddc33c4f2e6d93c7f9b3d57a1e8418233
WeeYoungSeok <dndrldzk@gmail.com>
2024-12-12 20:18:24 +0900

    feat : isRead가 없어질 경우를 대비한 알림 상태 controller 추가
    Fixes: #95

--- src/main/java/com/finalproject/manitoone/controller/api/NotificationApiController.java
+++ src/main/java/com/finalproject/manitoone/controller/api/NotificationApiController.java
@@ -4,6 +4,7 @@ import com.finalproject.manitoone.service.NotificationService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,5 +19,11 @@ public class NotificationApiController {
   public ResponseEntity readNotification(@PathVariable Long notiId, HttpServletRequest request) {
     notificationService.readNotification(notiId, request.getSession());
     return ResponseEntity.ok().build();
+  }
+
+  @GetMapping("/notifications/status")
+  public ResponseEntity<Object> checkUnreadNotifications(HttpServletRequest request) {
+    return ResponseEntity.ok(notificationService.hasUnreadNotifications(
+        (String) request.getSession().getAttribute("email")));
   }
 }

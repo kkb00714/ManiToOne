47e50c0fca65de8b5c7aabc215d46b88414b38c1
WeeYoungSeok <dndrldzk@gmail.com>
2024-12-12 21:05:28 +0900

    fix : @RequestMapping 수정
    Fixes: #95

--- src/main/java/com/finalproject/manitoone/controller/api/NotificationApiController.java
+++ src/main/java/com/finalproject/manitoone/controller/api/NotificationApiController.java
@@ -7,9 +7,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api")
+@RestController
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class NotificationApiController {
 

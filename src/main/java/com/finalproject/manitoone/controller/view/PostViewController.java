334f5724a5299ec5055892eaea83ba7655f67b8b
YoonjongJ <yoonjong.jang.123@gmail.com>
2024-12-12 13:37:04 +0900

    feat: 게시글 상세 페이지 타임리프 적용

--- src/main/java/com/finalproject/manitoone/controller/view/PostViewController.java
+++ src/main/java/com/finalproject/manitoone/controller/view/PostViewController.java
@@ -1,8 +1,13 @@
 package com.finalproject.manitoone.controller.view;
 
+import com.finalproject.manitoone.domain.dto.ReplyResponseDto;
 import com.finalproject.manitoone.service.PostService;
 import com.finalproject.manitoone.service.ReplyService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,8 +24,13 @@ public class PostViewController {
 
   // 게시글 상세 조회
   @GetMapping("/{postId}")
-  public String getPostDetail(@PathVariable("postId") Long postId, Model model) {
+  public String getPostDetail(@PathVariable("postId") Long postId,
+      @PageableDefault(sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
+      Model model) {
     model.addAttribute("post", postService.getPostDetail(postId));
+    model.addAttribute("postLikesNum", postService.getPostLikesNum(postId));
+    model.addAttribute("postRepliesNum", replyService.getRepliesNum(postId));
+    model.addAttribute("replies", replyService.getReplies(postId, pageable));
     return "pages/post/postDetail";
   }
 }

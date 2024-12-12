1bfeac3c4e89d05dd435042fccd9db8ff4f4bba4
WeeYoungSeok <dndrldzk@gmail.com>
2024-12-12 20:20:22 +0900

    feat : header.js로 isRead를 통한 알림 아이콘 변경
    Fixes: #95

---
+++ src/main/resources/static/script/header.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const isRead = localStorage.getItem("isRead");
+  const userEmail = document.querySelector('meta[name="email"]').getAttribute(
+      "content");
+  const notiImage = document.querySelector(".noti-image");
+
+  if (notiImage) {
+    if (isRead === null) {
+      if (userEmail !== "") {
+        // 서버 통신
+        fetch("/notifications/status")
+        .then(response => response.json())
+        .then(data => {
+          const hasUnread = data;
+          localStorage.setItem("isRead", hasUnread);
+          notiImage.src = hasUnread
+              ? "/images/icons/UI-notification2-on.png"
+              : "/images/icons/UI-notification2.png";
+        })
+        .catch(error => {
+          notiImage.src = "/images/icons/UI-notification2.png";
+        });
+      } else {
+        notiImage.src = "/images/icons/UI-notification2.png";
+      }
+    } else {
+      if (isRead) {
+        notiImage.src = "/images/icons/UI-notification2-on.png";
+      } else {
+        notiImage.src = "/images/icons/UI-notification2.png";
+      }
+    }
+  } else {
+    console.error("알림 아이콘을 찾을 수 없습니다.");
+  }
+});

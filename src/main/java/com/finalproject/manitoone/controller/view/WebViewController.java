f5c5a4041b9785ce973fd24a882af27a4541087c
geunah <geunah1109@gmail.com>
2024-12-12 09:11:35 +0900

    refactor : 주석처리 된 AuthenticationPrincipal 삭제, ReportObjectType에 마니또 편지/답변 세분화

--- src/main/java/com/finalproject/manitoone/controller/view/WebViewController.java
+++ src/main/java/com/finalproject/manitoone/controller/view/WebViewController.java
@@ -16,21 +16,6 @@ public class WebViewController {
 
   private final PostService postService;
 
-  // Spring Security @AuthenticationPrincipal을 이용한 인증
-//  @GetMapping("/")
-//  public String getIndex(Model model, @AuthenticationPrincipal UserDetails userDetails) {
-//    if (userDetails == null) {
-//      return "redirect:/login";
-//    }
-//    String nickname = userDetails.getUsername();
-//    model.addAttribute("userNickname", nickname);
-//    model.addAttribute("posts", postService.getTimelinePosts(
-//        nickname,
-//        PageRequest.of(0, 20, Sort.by(Direction.DESC, "createdAt"))
-//    ));
-//    return "index";
-//  }
-
   @GetMapping("/")
   public String getIndex(Model model, HttpSession session) {
     String nickname = (String) session.getAttribute("nickname");

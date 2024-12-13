89127acbb1aebb628ac33a65665e72ae1a0d58ec
geunah <geunah1109@gmail.com>
2024-12-13 10:40:06 +0900

    refactor : 세션 api용 SessionController 생성

---
+++ src/main/java/com/finalproject/manitoone/controller/api/SessionController.java
@@ -0,0 +1,24 @@
+package com.finalproject.manitoone.controller.api;
+
+import jakarta.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class SessionController {
+
+  @GetMapping("/session-info")
+  public ResponseEntity<Map<String, String>> getSessionInfo(HttpSession session) {
+    Map<String, String> response = new HashMap<>();
+    String nickname = (String) session.getAttribute("nickname");
+    response.put("nickname", nickname);
+    return ResponseEntity.ok(response);
+  }
+}
--- src/main/java/com/finalproject/manitoone/controller/view/WebViewController.java
+++ src/main/java/com/finalproject/manitoone/controller/view/WebViewController.java
@@ -2,17 +2,13 @@ package com.finalproject.manitoone.controller.view;
 
 import com.finalproject.manitoone.service.PostService;
 import jakarta.servlet.http.HttpSession;
-import java.util.HashMap;
-import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequiredArgsConstructor
@@ -32,14 +28,5 @@ public class WebViewController {
         PageRequest.of(0, 20, Sort.by(Direction.DESC, "createdAt"))
     ));
     return "index";
-  }
-
-  @GetMapping("/api/session-info")
-  @ResponseBody
-  public ResponseEntity<Map<String, String>> getSessionInfo(HttpSession session) {
-    Map<String, String> response = new HashMap<>();
-    String nickname = (String) session.getAttribute("nickname");
-    response.put("nickname", nickname);
-    return ResponseEntity.ok(response);
   }
 }

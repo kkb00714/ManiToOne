554418241320e6c375eb9f039b3198721abc292d
YoonjongJ <yoonjong.jang.123@gmail.com>
2024-12-13 14:21:05 +0900

    feat: 답글 좋아요 메소드 추가

--- src/main/resources/static/script/post.js
+++ src/main/resources/static/script/post.js
@@ -9,10 +9,32 @@ async function likePost(postId) {
     });
 
     if (response.ok) {
-      alert("게시글 ${postId}에 좋아요를 누르셨습니다.");
+      alert("해당 게시글에 좋아요를 누르셨습니다.");
     }
     else {
-      alert("좋아요 요청에 실패했습니다.")
+      alert("좋아요 요청에 실패하셨습니다.")
+    }
+  } catch (error) {
+    console.error("좋아요 요청 오류: ", error);
+    alert("좋아요 요청 중 오류가 발생했습니다.");
+  }
+}
+
+// 답글 좋아요
+async function likeReply(replyId) {
+  try {
+    const response = await fetch(`/api/reply/like/${replyId}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    if (response.ok) {
+      alert("해당 댓글에 좋아요를 누르셨습니다.");
+    }
+    else {
+      alert("좋아요 요청에 실패하셨습니다.")
     }
   } catch (error) {
     console.error("좋아요 요청 오류: ", error);

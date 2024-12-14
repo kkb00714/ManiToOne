5dbcd366809a167ececfc99c4701b08403bfd808
YoonjongJ <yoonjong.jang.123@gmail.com>
2024-12-15 02:47:35 +0900

    feat: WebViewController - getIndex 메소드에 User 객체 전달하기 추가

--- src/main/java/com/finalproject/manitoone/controller/view/WebViewController.java
+++ src/main/java/com/finalproject/manitoone/controller/view/WebViewController.java
@@ -1,6 +1,7 @@
 package com.finalproject.manitoone.controller.view;
 
 import com.finalproject.manitoone.service.PostService;
+import com.finalproject.manitoone.service.UserService;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class WebViewController {
 
   private final PostService postService;
+  private final UserService userService;
 
   @GetMapping("/")
   public String getIndex(Model model, HttpSession session) {
@@ -27,6 +29,7 @@ public class WebViewController {
         nickname,
         PageRequest.of(0, 20, Sort.by(Direction.DESC, "createdAt"))
     ));
+    model.addAttribute("user", userService.getCurrentUser(nickname));
     return "index";
   }
 }

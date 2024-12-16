494b7f1fd096fe11cb5415e6445f54126714ab85
WeeYoungSeok <dndrldzk@gmail.com>
2024-12-16 18:56:38 +0900

    feat : 메인 페이지 유저 검색 Dto 생성
    Fixes: #130

---
+++ src/main/java/com/finalproject/manitoone/dto/user/MainUserSearchResponseDto.java
@@ -0,0 +1,12 @@
+package com.finalproject.manitoone.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class MainUserSearchResponseDto {
+
+  private final String profileImage;
+  private final String nickname;
+}

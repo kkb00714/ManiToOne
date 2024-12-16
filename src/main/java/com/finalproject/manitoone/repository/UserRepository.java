0e99844bb01051038e9d79f3d99950b419b8c240
WeeYoungSeok <dndrldzk@gmail.com>
2024-12-16 18:56:56 +0900

    feat : 메인 페이지 유저 검색 repo 작성
    Fixes: #130

--- src/main/java/com/finalproject/manitoone/repository/UserRepository.java
+++ src/main/java/com/finalproject/manitoone/repository/UserRepository.java
@@ -1,8 +1,13 @@
 package com.finalproject.manitoone.repository;
 
 import com.finalproject.manitoone.domain.User;
+import com.finalproject.manitoone.dto.user.MainUserSearchResponseDto;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -19,4 +24,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
   boolean existsByNickname(String nickname);
 
   User findOAuth2ByEmail(String email);
+
+  @Query("SELECT new com.finalproject.manitoone.dto.user.MainUserSearchResponseDto(u.profileImage, u.nickname) " +
+      "FROM User u WHERE u.nickname LIKE :query%")
+  Page<MainUserSearchResponseDto> findUsersByNicknameStartsWith(@Param("query") String query, Pageable pageable);
 }

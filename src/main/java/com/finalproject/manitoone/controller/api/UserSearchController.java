945a44e574f66877c1a0b305d8b7e10c3de3b910
WeeYoungSeok <dndrldzk@gmail.com>
2024-12-16 18:57:28 +0900

    feat : 메인 페이지 유저 검색 controller 구현
    Fixes: #130

--- src/main/java/com/finalproject/manitoone/controller/api/UserSearchController.java
+++ src/main/java/com/finalproject/manitoone/controller/api/UserSearchController.java
@@ -1,10 +1,30 @@
 package com.finalproject.manitoone.controller.api;
 
+import com.finalproject.manitoone.dto.user.MainUserSearchResponseDto;
+import com.finalproject.manitoone.service.UserSearchService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
 public class UserSearchController {
 
+  private final UserSearchService userSearchService;
+
+  @GetMapping("/user/search")
+  public ResponseEntity<Object> getUsers(
+      @RequestParam(required = false, defaultValue = "") String query,
+      @PageableDefault(size = 7, sort = "userId", direction = Sort.Direction.ASC) Pageable pageable) {
+    Page<MainUserSearchResponseDto> userPage = userSearchService.searchUsers(query, pageable);
+    return ResponseEntity.ok(userPage);
+  }
 }

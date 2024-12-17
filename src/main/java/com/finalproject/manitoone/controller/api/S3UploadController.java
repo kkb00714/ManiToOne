80d72d64bb1d464ac84f8bb79570ba6e8e8684b4
kkb <0furiousblade0@gmail.com>
2024-12-17 21:26:01 +0900

    chore : 이미지 변경 컨트롤러 추가 #131

--- src/main/java/com/finalproject/manitoone/controller/api/S3UploadController.java
+++ src/main/java/com/finalproject/manitoone/controller/api/S3UploadController.java
@@ -2,6 +2,7 @@ package com.finalproject.manitoone.controller.api;
 
 import com.finalproject.manitoone.service.S3Service;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,6 +24,19 @@ public class S3UploadController {
       return "파일 업로드 성공!" + imageUrl;
     } catch (Exception e) {
       return e.getMessage();
+    }
+  }
+
+  @PostMapping("/update-profile-image")
+  public ResponseEntity<String> updateProfileImage(
+      @RequestParam("email") String email,
+      @RequestParam("file") MultipartFile file) {
+
+    try {
+      String newImageUrl = s3Service.updateProfileImage(email, file);
+      return ResponseEntity.ok("프로필 사진이 업데이트되었습니다: " + newImageUrl);
+    } catch (Exception e) {
+      return ResponseEntity.status(500).body("프로필 사진 업데이트 실패: " + e.getMessage());
     }
   }
 }

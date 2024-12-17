5a22eb4a070eb75f4c70d35a978a4ca62e04c8f2
WeeYoungSeok <dndrldzk@gmail.com>
2024-12-17 23:04:20 +0900

    feat : ai 피드백 repo 작성
    Fixes: #170

--- src/main/java/com/finalproject/manitoone/repository/AiPostLogRepository.java
+++ src/main/java/com/finalproject/manitoone/repository/AiPostLogRepository.java
@@ -1,6 +1,7 @@
 package com.finalproject.manitoone.repository;
 
 import com.finalproject.manitoone.domain.AiPostLog;
+import com.finalproject.manitoone.domain.User;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,4 +10,5 @@ import org.springframework.stereotype.Repository;
 public interface AiPostLogRepository extends JpaRepository<AiPostLog, Long> {
 
   Optional<AiPostLog> findByPostPostId(Long postId);
+  Optional<AiPostLog> findTopByPost_UserAndAiContentIsNotNullOrderByPost_CreatedAtDesc(User user);
 }

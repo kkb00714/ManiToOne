88a3d31c3d24c9b037437efdd2bf980578098df3
kkb <0furiousblade0@gmail.com>
2024-12-17 22:24:43 +0900

    fix : not blank 속성 제거 #131

--- src/main/java/com/finalproject/manitoone/domain/dto/UserUpdateDto.java
+++ src/main/java/com/finalproject/manitoone/domain/dto/UserUpdateDto.java
@@ -24,11 +24,9 @@ public class UserUpdateDto {
       regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,16}$",
       message = "비밀번호는 영어, 숫자, 특수문자를 포함하여 8~16자리로 설정해야 합니다."
   )
-  @NotBlank(message = "비밀번호는 필수 입력값입니다.")
   private String password;
 
   @Size(min = 2, max = 10, message = "닉네임은 2~10자리로 설정해야 합니다.")
-  @NotBlank(message = "닉네임은 필수 입력값입니다.")
   @Pattern(regexp = "^[a-zA-Z0-9가-힣]{2,10}$", message = "닉네임은 알파벳 대소문자와 숫자만 포함할 수 있습니다.")
   private String nickname;
 

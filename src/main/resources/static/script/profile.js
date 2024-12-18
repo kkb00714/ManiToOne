300160a71bf2ed45bf49022f2102478e27ce6a09
kkb <0furiousblade0@gmail.com>
2024-12-18 20:47:03 +0900

    fix : 파일 용량 제한 설정추가 #131

--- src/main/resources/static/script/profile.js
+++ src/main/resources/static/script/profile.js
@@ -310,9 +310,16 @@ document.addEventListener("DOMContentLoaded", function () {
         profileImageInput.value = "";
         return;
       }
+      const maxSizeMB = 5;
+      if (file.size > maxSizeMB * 1024 * 1024) {
+        alert(`파일 크기는 ${maxSizeMB}MB 이하로만 선택할 수 있습니다.`);
+        profileImageInput.value = "";
+
+        return;
+      }
       updateProfileImage(file);
     }
-  })
+  });
 
   profileImage.addEventListener("click", () => {
     modal.style.display = "block";

2f89bb816700711bae81902ca50712674bb33d36
kkb <0furiousblade0@gmail.com>
2024-12-18 20:24:45 +0900

    fix : 이미지 업로드 제한 #131

--- src/main/resources/static/script/profile.js
+++ src/main/resources/static/script/profile.js
@@ -302,6 +302,18 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
+  profileImageInput.addEventListener("change", (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      if (!file.type.match(/^image\/(png|jpe?g)$/)) {
+        alert("PNG, JPG, JPEG 파일만 선택할 수 있습니다.");
+        profileImageInput.value = "";
+        return;
+      }
+      updateProfileImage(file);
+    }
+  })
+
   profileImage.addEventListener("click", () => {
     modal.style.display = "block";
     modalOverlay.style.display = "block";
